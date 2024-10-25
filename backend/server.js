49adddd487aef5b969ad9ae0b5a32115928ab6bf
muradmy00 <murad.my00@gmail.com>
2024-10-26 02:07:36 +0600

    pro

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js' 
+import adminRouter from './routes/adminRoute.js'
+
+//app config
+
+const app = express()
+
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+
+
+//Middlewares
+
+app.use(express.json())
+app.use(cors())
+
+
+//api endpoints
+
+app.use('/api/admin' , adminRouter)
+
+
+app.get('/',(req,res)=>{
+ 
+    res.send("API Working with nodemon")
+})
+
+app.listen(port , ()=> console.log("Server Started",port))
