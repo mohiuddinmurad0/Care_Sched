f4ad312b854d147bae81688158dfc5d6da69127c
muradmy00 <murad.my00@gmail.com>
2024-11-23 03:19:20 +0600

    Nov-23

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -9,7 +9,15 @@ const MyAppointments = () => {
 
   const [appointments,setAppointments] = useState([])
 
-  const months = ["Jan", "Feb", "Mar", "Apr", ""]
+  const months = ["","Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+  const slotDateFormat = (slotDate) => {
+
+    const dateArray = slotDate.split('_')
+
+    return dateArray[0]+ " " + months[Number(dateArray[1])] + " " + dateArray[2]
+
+  }
 
   const getUserAppointments = async () => {
 
@@ -19,7 +27,7 @@ const MyAppointments = () => {
 
       if (data.success) {
         setAppointments(data.appointments.reverse())
-        console.log(data.appointments)
+        //console.log(data.appointments)
       }
 
 
@@ -58,7 +66,7 @@ const MyAppointments = () => {
               <p className='text-zinc-700 font-medium mt-1'>Address:</p>
                 <p className='text-xs'>{item.docData.address.line1}</p>
                 <p className='text-xs'>{item.docData.address.line2}</p>
-              <p className='text-xs mt-1'><span className='text-sm text-neutral-700 font-medium'>Date & Time:</span> {item.slotDate} | {item.slotTime}</p>
+              <p className='text-xs mt-1'><span className='text-sm text-neutral-700 font-medium'>Date & Time:</span> {slotDateFormat(item.slotDate)} | {item.slotTime}</p>
             </div>
             <div></div>
             <div className='flex flex-col gap-2 justify-end'>
